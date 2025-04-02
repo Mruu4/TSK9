2346409f4f8549a5d4dbce454468529f45af8251
Mruu4 <a55957b@gmail.com>
2025-04-02 16:02:41 +0300

    commit #4
    In the NV lines 10 and 11. Fix currency pair type (USD_NGN → USD_NGN) and update exchange rates and the other way round

--- module1/src/main/java/rates/Rates.java
+++ module1/src/main/java/rates/Rates.java
@@ -7,7 +7,8 @@ public class Rates {
     private static final Map<String, Double> data = new HashMap<>();
 
     static {
-
+        data.put("NGN_USD", 0.0012); // 1 Naira = 0.0012 USD
+        data.put("USD_NGN", 830.0);  // 1 USD = 830 Naira
     }
 
     public static double get(String from, String to) {
